9f3713f37829eab3e18b960d964fa718cabe49db
jannisuhlendorf <jannis.uhlendorf@hu-berlin.de>
2017-06-21 17:39:07 +0200

    alsmost working clean up

--- fit_mother_and_bud.py
+++ fit_mother_and_bud.py
@@ -13,35 +13,82 @@ def volume_to_radius(volume):
     r = (3./4/np.pi*volume) ** (1./3)
     return r
 
+def get_additional_model_parameters(data):
+  assert data['time'][0] == 0
+  budding_start = data_trunc['time'][~np.isnan(data_trunc['bud_V_tot_fl'])][0]
+  volume_mother_0 = data_trunc['mother_V_tot_fl'][0]
+  volume_bud_0 = data_trunc['bud_V_tot_fl'][~np.isnan(data_trunc['bud_V_tot_fl'])][0]
+  r_tot_mother = volume_to_radius(volume_mother_0)
+  r_tot_bud = volume_to_radius(volume_bud_0)
+  r_os_mother = r_tot_mother - model['mother_r_b']
+  r_os_bud = r_tot_bud - model['bud_r_b']   
+  additional_model_parameters = { 'mother_r_os': r_os_mother,
+                                  'bud_r_os': r_os_bud,                                   
+                                  'budding_start': budding_start }     
+  return additional_model_parameters
 
-def fit_single_mother_and_bud(data, model, parameters_to_fit, 
-                                additional_concentrations, additional_model_parameters):
-    mothercells_data, daughtercells_data, time_data = model_data.load_data()
-    time_data = time_data * 60
-    model = simulate.load_model('volume_mother_and_bud.txt')
+
+def fit_single_mother_and_bud(model,
+                              data,
+                              parameters_to_fit, 
+                              additional_concentrations={}):
+  data_trunc = model_data.truncate_data(data)
+  data_trunc['time'] = data_trunc['time'] + abs(min(data_trunc['time']))
+  additional_model_parameters = get_additional_model_parameters(data_trunc)
+  fitted_parameters = fit_data.fit_model_to_data(model, 
+                                                 data_trunc, 
+                                                 parameters_to_fit,   
+                                                 'cmaes', 
+                                                 additional_model_parameters=additional_model_parameters,
+                                                 additional_concentrations=additional_concentrations)
+
+def fit_all_mother_bud(model,
+                       mothercells_data,
+                       daughtercells_data,
+                       time_data,
+                       parameters_to_fit,
+                       additional_concentrations={}):
+  assert len(mothercells_data) == len(daughtercells_data)
+  df_params = pd.DataFrame(columns=parameters_to_fit)
+  for cell_id in len(mothercells_data):
+    print 'fitting cell no %d out of %d' %(cell_id, len(mothercells_data))
+    data = {'time': np.array(time_data), 
+            'mother_V_tot_fl': mothercells_data[cell_id, :],
+            'bud_V_tot_fl': daughtercells_data[cell_id, :]}
+    fitted_parameters = fit_single_mother_and_bud(model, data, parameters_to_fit, additional_concentrations)
+    df_params.loc[len(df_params)] = fitted_parameters
+  return df_params
 
 
-    pass
-
+    
 
 
 if __name__ == '__main__':
     mothercells_data, daughtercells_data, time_data = model_data.load_data()
     
-    time_data = time_data * 60
-    
-    #plot_single_cell(time_data, mothercells_data[0, :], daughtercells_data[0, :])
-    #plot_data(mothercells_data, daughtercells_data, time_data)
-
+    time_data = time_data * 60 # convert time to seconds
     model = simulate.load_model('volume_mother_and_bud.txt')
-    #model = model_data.set_model_parameters(model, {'k_nutrient': 0})
-    #model = model_data.select_model_timecourses(['V_tot_fl'])
-    #simulation_result = simulate.simulate_model(model, end_time=7200)
-    #simulate.plot((simulation_result,), legend=True)
-
     
     parameters_to_fit = ['k_nutrient', 'k_deg','mother_phi','bud_phi']
     
+
+    additional_concentrations = {'[mother_c_i]': 325,
+                                 '[bud_c_i]': 325 }
+
+    df_params = fit_all_mother_bud(model, 
+                                   mothercells_data, 
+                                   daughtercells_data, 
+                                   time_data, 
+                                   parameters_to_fit, 
+                                   additional_concentrations)
+
+
+    sdf
+
+
+
+
+    sdf
     cell_ids = np.size(mothercells_data[:,1])
     fit_para_list=[]
 
